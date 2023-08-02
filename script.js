5108e93d307980246a4e511e4b2a3bf0646ceb02
satej temkar <satejstemkar2177@gmail.com>
2023-08-02 12:30:46 +0530

    portfolio

---
+++ script.js
@@ -0,0 +1,72 @@
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a')
+
+window.onscroll = () => {
+    sections.forEach(sec =>{
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            })
+        }
+    })
+
+
+    let header = document.querySelector('header');
+    
+    header.classList.toggle('sticky', window.scrollY > 1);
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
+   
+}
+
+
+// emailjs
+
+
+function sendmail(){
+    var params = {
+        name: document.getElementById("name").value ,
+        email: document.getElementById("email").value ,
+        number: document.getElementById("number").value ,
+        city: document.getElementById("city").value ,
+        message: document.getElementById("message").value 
+
+    };
+
+    const serviceID = "service_fcii2s3";
+    const templateID = "template_vvwaxpc";
+
+    emailjs.send(serviceID, templateID, params)
+    .then(
+        res => {
+            document.getElementById("name").value ="";
+            document.getElementById("email").value ="";
+            document.getElementById("number").value ="";
+            document.getElementById("city").value ="";
+            document.getElementById("message").value ="";
+            console.log(res);
+            alert(" Your message sent successfully....")
+        }
+    )
+    .catch(err => console.log(err));
+}
+
